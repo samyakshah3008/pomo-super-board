609cf0f0c7ab462ff15e32ae912b650262c1ffba
Samyak Shah <samyakshah3008@gmail.com>
2024-08-16 22:50:34 +0530

    feat: tooltip add for button

--- app/(dashboard)/_components/sidebar/create-new-org-button.tsx
+++ app/(dashboard)/_components/sidebar/create-new-org-button.tsx
@@ -3,6 +3,7 @@
 import { CreateOrganization } from "@clerk/nextjs";
 import { Plus } from "lucide-react";
 
+import Hint from "@/components/hint";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 
 const CreateNewOrgButton = () => {
@@ -10,9 +11,16 @@ const CreateNewOrgButton = () => {
     <Dialog>
       <DialogTrigger asChild>
         <div className="aspect-square">
-          <button className="bg-white/25 h-full w-full rounded-md flex items-center justify-center opacity-60 hover:opactiy-100 transition">
-            <Plus className="text-white" />
-          </button>
+          <Hint
+            label="Create organization"
+            side="right"
+            align="start"
+            sideOffset={18}
+          >
+            <button className="bg-white/25 h-9 w-9 rounded-md flex items-center justify-center opacity-60 hover:opactiy-100 transition">
+              <Plus className="text-white" />
+            </button>
+          </Hint>
         </div>
       </DialogTrigger>
       <DialogContent className="p-0 bg-transparent border-none max-w-[480px]">
