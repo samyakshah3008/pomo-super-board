b401a8090f684d6161aac3c0f0f33e3c14ef0422
Samyak Shah <samyakshah3008@gmail.com>
2024-08-16 22:47:50 +0530

    feat: dashboard root page add

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,3 +1,26 @@
-export default function Dashboard() {
-  return <div>This is a screen for Authenticated Users only</div>;
+"use client";
+
+import { useOrganization } from "@clerk/nextjs";
+import BoardList from "./_components/board-list";
+import EmptyOrg from "./_components/empty-org";
+
+interface DashboardPageProps {
+  searchParams: {
+    search?: string;
+    favorites?: string;
+  };
+}
+
+export default function Dashboard({ searchParams }: DashboardPageProps) {
+  const { organization } = useOrganization();
+
+  return (
+    <div className="flex-1 h-[calc(100%-80px)] p-6">
+      {!organization ? (
+        <EmptyOrg />
+      ) : (
+        <BoardList orgId={organization.id} query={searchParams} />
+      )}
+    </div>
+  );
 }
