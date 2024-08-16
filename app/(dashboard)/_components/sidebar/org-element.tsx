30beeddad75ba3b1096223cb6e69d6393b6b2512
Samyak Shah <samyakshah3008@gmail.com>
2024-08-16 22:50:51 +0530

    tooltip add for org element

--- app/(dashboard)/_components/sidebar/org-element.tsx
+++ app/(dashboard)/_components/sidebar/org-element.tsx
@@ -1,3 +1,4 @@
+import Hint from "@/components/hint";
 import { cn } from "@/lib/utils";
 import { useOrganization, useOrganizationList } from "@clerk/nextjs";
 import Image from "next/image";
@@ -14,23 +15,25 @@ const OrgElement = ({ id, name, imageUrl }: OrgElementProps) => {
 
   const isActive = organization?.id == id;
 
-  const onClick = () => {
+  const onOrgElementClick = () => {
     if (!setActive) return null;
     setActive({ organization: id });
   };
 
   return (
     <div className="aspect-square relative">
-      <Image
-        alt={name}
-        src={imageUrl}
-        onClick={() => {}}
-        fill
-        className={cn(
-          "rounded-md cursor-pointer opacity-75 hover:opacity-100 transition",
-          isActive && "opacity-100"
-        )}
-      />
+      <Hint label={name} side="right" align="center" sideOffset={52}>
+        <Image
+          alt={name}
+          src={imageUrl}
+          onClick={onOrgElementClick}
+          fill
+          className={cn(
+            "rounded-md cursor-pointer opacity-75 hover:opacity-100 transition",
+            isActive && "opacity-100"
+          )}
+        />
+      </Hint>
     </div>
   );
 };
