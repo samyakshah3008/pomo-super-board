b2539e12f88d3ce93de10c0b5eed8aecaddca915
Samyak Shah <samyakshah3008@gmail.com>
2024-08-21 17:53:26 +0530

    feat: board card add

---
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import Actions from "@/components/actions";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useAuth } from "@clerk/nextjs";
+import { formatDistanceToNow } from "date-fns";
+import { MoreHorizontal } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import Footer from "./footer";
+import Overlay from "./overlay";
+
+interface BoardCardProps {
+  key: string;
+  id: string;
+  title: string;
+  imageUrl: string;
+  authorId: string;
+  authorName: string;
+  createdAt: number;
+  orgId: string;
+  isFavorite: boolean;
+}
+
+const BoardCard = ({
+  key,
+  id,
+  title,
+  imageUrl,
+  authorId,
+  authorName,
+  createdAt,
+  orgId,
+  isFavorite,
+}: BoardCardProps) => {
+  const { userId } = useAuth();
+  const authorLabel = userId == authorId ? "You" : authorName;
+  const createdAtLabel = formatDistanceToNow(createdAt, { addSuffix: true });
+
+  return (
+    <Link href={`/board/${id}`}>
+      <div className="group aspect-[100/127] border rounded-lg flex flex-col justify-between overflow-hidden ">
+        <div className="relative flex-1 bg-amber-50">
+          <Image src={imageUrl} alt="doodle" fill className="object-fill" />
+          <Overlay />
+          <Actions id={id} title={title} side="right">
+            <button className="absolute top-1 right-1 opacity-0 group-hover:opacity-100 transition-opacity px-3 py-2 outline-none">
+              <MoreHorizontal className="text-white opacity-75 hover:opacity-100 transition-opacity" />
+            </button>
+          </Actions>
+        </div>
+        <Footer
+          title={title}
+          createdAtLabel={createdAtLabel}
+          isFavorite={isFavorite}
+          authorLabel={authorLabel}
+          onClick={() => {}}
+          disabled={false}
+        ></Footer>
+      </div>
+    </Link>
+  );
+};
+
+export const SkeletonBoardCard = () => {
+  return (
+    <div className="group aspect-[100/127] rounded-lg overflow-hidden ">
+      <Skeleton className="h-full w-full" />
+    </div>
+  );
+};
+
+export default BoardCard;
