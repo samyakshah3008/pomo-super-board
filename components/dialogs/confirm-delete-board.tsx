97ac399fc70160eb039a6f1bf762d81064f4049b
Samyak Shah <samyakshah3008@gmail.com>
2024-08-21 17:55:18 +0530

    feat: board card copy delete rename actions add

---
+++ components/dialogs/confirm-delete-board.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "../ui/alert-dialog";
+
+interface ConfirmDeleteBoardProps {
+  children: React.ReactNode;
+  onConfirm: () => void;
+  disabled?: boolean;
+  header: string;
+  description?: string;
+}
+
+const ConfirmDeleteBoard = ({
+  children,
+  onConfirm,
+  disabled,
+  header,
+  description,
+}: ConfirmDeleteBoardProps) => {
+  const handleConfirm = () => {
+    onConfirm();
+  };
+
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>{children}</AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>
+            {header}
+            <AlertDialogDescription className="mt-2 ">
+              {description}
+            </AlertDialogDescription>
+          </AlertDialogTitle>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction disabled={disabled} onClick={handleConfirm}>
+            Confirm
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
+
+export default ConfirmDeleteBoard;
