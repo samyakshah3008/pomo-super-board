4c3fc46a397300b316be107f7a479965a91c171b
Samyak Shah <samyakshah3008@gmail.com>
2024-08-22 16:39:25 +0530

    feat: favorite unfavorite business logic add

--- app/(dashboard)/_components/board-card/index.tsx
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -2,11 +2,13 @@
 
 import Actions from "@/components/actions";
 import { Skeleton } from "@/components/ui/skeleton";
+import { api } from "@/convex/_generated/api";
+import { useApiMutation } from "@/hooks/use-api-mutation";
 import { useAuth } from "@clerk/nextjs";
 import { formatDistanceToNow } from "date-fns";
 import { MoreHorizontal } from "lucide-react";
 import Image from "next/image";
-import Link from "next/link";
+import { toast } from "sonner";
 import Footer from "./footer";
 import Overlay from "./overlay";
 
@@ -37,8 +39,30 @@ const BoardCard = ({
   const authorLabel = userId == authorId ? "You" : authorName;
   const createdAtLabel = formatDistanceToNow(createdAt, { addSuffix: true });
 
+  const { mutate: onFavorite, pending: pendingFavorite } = useApiMutation(
+    api.board.favorite
+  );
+  const { mutate: onUnfavorite, pending: pendingUnfavorite } = useApiMutation(
+    api.board.unfavorite
+  );
+
+  const toggleFavorite: any = (e: any) => {
+    e.stopPropagation();
+    if (isFavorite) {
+      onUnfavorite({ id }).catch(() => {
+        toast.error("Failed to unfavorite, please try again later.");
+      });
+    } else {
+      onFavorite({ id, orgId }).catch(() => {
+        toast.error("Failed to favorite, please try again later.");
+      });
+    }
+  };
+
   return (
-    <Link href={`/board/${id}`}>
+    <div
+    // href={`/board/${id}`}
+    >
       <div className="group aspect-[100/127] border rounded-lg flex flex-col justify-between overflow-hidden ">
         <div className="relative flex-1 bg-amber-50">
           <Image src={imageUrl} alt="doodle" fill className="object-fill" />
@@ -54,11 +78,11 @@ const BoardCard = ({
           createdAtLabel={createdAtLabel}
           isFavorite={isFavorite}
           authorLabel={authorLabel}
-          onClick={() => {}}
-          disabled={false}
+          onClick={toggleFavorite}
+          disabled={pendingFavorite || pendingUnfavorite}
         ></Footer>
       </div>
-    </Link>
+    </div>
   );
 };
 
