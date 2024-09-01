7ea12273ccc089635e25935360812974d75f7a45
Samyak Shah <samyakshah3008@gmail.com>
2024-09-01 16:08:23 +0530

    feat: skeletons and loaders add

--- app/board/[boardId]/_components/participants.tsx
+++ app/board/[boardId]/_components/participants.tsx
@@ -7,3 +7,9 @@ const Participants = () => {
 };
 
 export default Participants;
+
+Participants.Skeleton = function ParticipantsSkeleton() {
+  return (
+    <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md w-[100px]  "></div>
+  );
+};
