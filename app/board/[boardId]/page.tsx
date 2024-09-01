dea99a1929fa70eb4b513f978faf156cd80556c5
Samyak Shah <samyakshah3008@gmail.com>
2024-09-01 16:07:58 +0530

    feat: board id dynamic route page add

--- app/board/[boardId]/page.tsx
+++ app/board/[boardId]/page.tsx
@@ -1,4 +1,8 @@
+"use client";
+
+import Room from "@/components/room";
 import Canvas from "./_components/canvas";
+import Loading from "./_components/loading";
 
 interface BoardIdPageProps {
   params: {
@@ -8,9 +12,9 @@ interface BoardIdPageProps {
 
 const BoardIdPage = ({ params }: BoardIdPageProps) => {
   return (
-    <div>
+    <Room roomId={params.boardId} fallback={<Loading />}>
       <Canvas boardId={params.boardId} />
-    </div>
+    </Room>
   );
 };
 
